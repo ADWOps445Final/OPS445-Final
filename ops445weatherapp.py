70e9e876f5992b994c5c5f98eae30e2547c1c188
ADWOps445Final <161248806+ADWOps445Final@users.noreply.github.com>
2024-03-21 18:20:21 -0400

    Update ops445weatherapp.py
    Added get_weather function

--- ops445weatherapp.py
+++ ops445weatherapp.py
@@ -40,3 +40,26 @@ config_file = 'config.ini'
 config = ConfigParser()
 config.read(config_file)
 api_key = config['api_key']['key']
+
+"""
+The purpose of the function get_weather is to create and send the HTTP request for the weather
+ of the location provided by the end-user. After the connection is established and an HTTP request
+ is made, a reply is sent from OpenWeatherMap to retrieve the data. The function then processes
+ the data from the reply, and retrives the relevant data regarding the weather of the location. 
+ The data is then stored into variables to be used for the variables.  
+"""  
+def get_weather(city):
+    result = requests.get(url.format(city, api_key))
+    if result.status_code == 200:
+        json = result.json()
+        city = json['name']
+        country = json['sys']['country']
+        temp_kelvin = json['main']['temp']
+        temp_celsius = temp_kelvin - 273.15
+        temp_fahrenheit = (temp_kelvin - 273.15) * 9 / 5 + 32
+        icon = json['weather'][0]['icon']
+        weather = json['weather'][0]['main']
+        final = (city, country, temp_celsius, temp_fahrenheit, icon, weather)
+        return final
+    else:
+        return None
